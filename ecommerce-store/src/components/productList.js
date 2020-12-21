b7b6372431e55ab53f5037ca8f658ce10b9dd8b1
Brandon Curtis <Brandcurtis43@gmail.com>
2020-12-21 11:33:39 -0700

    Added components to render products on the page. (Needs to use npm i to install extensions used)

---
+++ ecommerce-store/src/components/productList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Product from './product'
+
+const ProductList = ({productData}) => {
+    return (
+        <div>
+            {productData.map((product, index) => 
+                <Product 
+                    key={index}
+                    img={product.img}
+                    title={product.title}
+                    price={product.price}
+                    rating={product.rating}
+                />
+            )}
+        </div>
+    )
+}
+
+export default ProductList
--- ecommerce-store/src/views/loginPage.js
+++ ecommerce-store/src/views/loginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function LoginPage() {
+const LoginPage = () => {
     return (
         <h1>Placeholder Test</h1>
     )
